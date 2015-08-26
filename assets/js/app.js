ab47cba88aea032abf5bc6038634d29037007608
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-08-26 16:28:48 -0700

    add assets on client side

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+angular.module('myApp', [ 
+        'ngResource',
+        'ngRoute',
+        'ngCookies',
+        'Nto1Factory',
+        'angularjs-dropdown-multiselect',
+        'ui.bootstrap',
+]);
