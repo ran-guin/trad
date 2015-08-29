3ff9068cfa5bf511dc8335e84290c9e8287b1ff4
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-08-28 18:05:00 -0700

    adjust patient & travel info on visit page

--- assets/js/private/visit/VisitController.js
+++ assets/js/private/visit/VisitController.js
@@ -44,7 +44,8 @@ app.controller('VisitController',
             { field : 'notes'},
             { field : 'treatment.id', hidden: 1},
             { field : 'contraindication.condition', label: 'Contraindications' },
-            { field : 'known_side_effect' }
+            { field : 'known_side_effect' },
+            { field : 'recommendationLevel', label: 'recommendation'}
        ];
     
         /* Load Fields based on fields above using tables / condition below */
@@ -65,16 +66,16 @@ app.controller('VisitController',
             url : '/api/search',
             view: 'visit/Visit',
             target : 'Vaccine',
-            show : "Disease, Vaccine, Contraindications, known_side_effect",
-            search : "Disease, Vaccine, Contraindications, known_side_effect",
+            show : "Vaccine, Disease, Contraindications, known_side_effect, recommendation",
+            search : "Vaccine, Disease, Contraindications, known_side_effect, recommendation",
             hide: 'id',
 
             query_table : "(vaccine,  disease_vaccines__vaccine_diseases as DV, disease) LEFT JOIN contraindication ON contraindication.vaccine_id=vaccine.id LEFT JOIN side_effect ON side_effect.vaccine_id=vaccine.id",
-            query_field : "disease.name as Disease, vaccine.name as Vaccine, contraindication.condition as Contraindications, known_side_effect",
+            query_field : "disease.name as Disease, vaccine.name as Vaccine, contraindication.condition as Contraindications, known_side_effect, recommendationLevel as recommendation",
             query_condition : "DV.vaccine_diseases=disease.id and DV.disease_vaccines=vaccine.id",
             
             // query : "SELECT DISTINCT User_Name,Request_Date,Item_Request_ID,Item_Category_Description,Unit_Qty,Item_Name,Item_Catalog,Vendor_ID,Vendor_Name, CASE WHEN Unit_Cost IS NULL THEN Item_Cost ELSE Unit_Cost END as Unit_Cost,Item_Request_Notes,Deliver_To, Item_Request_Notes FROM (Item, Item_Request, Request, User) JOIN Item_Category ON FK_Item_Category__ID=Item_Category_ID LEFT JOIN Vendor ON Vendor_ID=FK_Vendor__ID WHERE FK_Request__ID=Request_ID AND FKRequester_User__ID=User_ID AND FK_Item__ID=Item_ID AND Request_ID=FK_Request__ID",
-            set : "Disease, Vaccine, Contraindications, known_side_effect",
+            set : "Vaccine, Disease, Contraindications, known_side_effect,recommendation",
             // condition : "FK_Item_Category__ID IN (<Item_Category>)",
             onEmpty : "No Vaccine found.<P><div class='alert alert-warning'>Please try different spellings or different field to search.<P>Please only add a new item if this item has never been received before.  <button class='btn btn-primary' type='button' data-toggle='modal' data-target='#newPatientModal'> Add New Patient </button></div>\n"
         };
@@ -109,7 +110,7 @@ $scope.$parent.MenuSettings = {
 
     $scope.initialize = function( config ) {
 
-        console.log("local init: " + config);
+        console.log("local init: " + JSON.stringify(config));
         
         if (config && config['me']) { 
             console.log("loaded user attributes");
@@ -118,6 +119,10 @@ $scope.$parent.MenuSettings = {
         if (config && config['clinic']) { 
             console.log("loaded clinic attributes");
             $scope.clinic = config['clinic'];
+        }
+        if (config && config['patient']) { 
+            console.log("loaded patient attributes");
+            $scope.patient = config['patient'];
         }
 
         $scope.$parent.highlightBackground = "background-color:#9C9;";
@@ -153,21 +158,105 @@ $scope.$parent.MenuSettings = {
         Nto1Factory.addItem( $scope.itemColumns, $scope.items );
         var index = $scope.items.length - 1;
         console.log('added treatment...');
-        $scope.$parent.items[index].status = 'scheduled';
+        $scope.$parent.items[index].status = 'requested';
 
+    }
+
+    $scope.loadTravel = function () {
+        console.log("load travel plans...");
+        var travel = [
+            {
+                'region' : 'South America',
+                'start'  : '2015-09-01',
+                'finish' : '2015-10-01',
+            },{
+                'region' : 'Spain',
+                'start'  : '2016-02-01',
+            }
+        ];
+        $scope.travel = travel;
     }
 
     $scope.addBarcodedVaccine = function () {
         var barcode = document.getElementById('barcode');
         console.log("Barcode: " + barcode);
 
-        var example = {
+        $scope.loadExamples(['scanned','scanned'],[null,null], ['Recommended','Mandatory for Region'],1);
+ 
+    }
+
+    $scope.loadQueue = function () {
+        console.log("LOAD QUEUE");
+        var queueExample = [
+            {
+                id: 1,
+                patient: 'Ryan Reynolds',
+                age: 55,
+            },
+            {
+                id: 2,
+                patient: 'Brenda Reynolds',
+                age: 35,
+            },            
+
+        ];
+
+        $scope.queued = queueExample;
+    }
+
+    $scope.loadScheduledVaccinations = function () {
+        console.log("Retrieve suggested vaccinations from CDC API (?)");
+ 
+        $scope.loadExamples(['scheduled','scheduled'], ['due','overdue'],['Recommended','Mandatory for Region'],0);
+    }
+
+    $scope.loadExamples = function (status, due, recommendation, replace) {
+        var example1 = {
             'Disease' : 'HepA',
             'Vaccine' : "Example Vaccine",
             'lot' : '1234',
-            'known_side_effect' : "Nausea"
+            'known_side_effect' : "Nausea",
+            'status' : status[0],
+            'due' : due[0],
+            'recommendation' : recommendation[0],
         };
-        $scope.$parent.items.push(example);
+        $scope.applyVaccine(example1, replace);
+
+        var example2 = {
+            'Disease' : 'Yellow Fever',
+            'Vaccine' : "Yellow Fever vaccine",
+            'known_side_effect' : "Nausea",
+            'Contraindications' : 'Pregnancy',
+            'status' : status[1],
+            'due' : due[1],
+            'recommendation' : recommendation[1],
+        };
+
+        $scope.applyVaccine(example2, replace);
+    }
+
+    $scope.applyVaccine = function (vaccine, replace) {
+
+        var alreadyTracked = null;
+        for (var i=0; i<$scope.items.length; i++) {
+            console.log("Compare " + $scope.items[i]['Vaccine'] + ' with ' + vaccine['Vaccine']);
+            if ($scope.items[i]['Vaccine'] == vaccine['Vaccine']) {
+                alreadyTracked = i;
+            }
+        }
+        if (alreadyTracked == null) {
+            console.log("Add new vaccine: " + JSON.stringify(vaccine));
+            $scope.$parent.items.push(vaccine);
+        }
+        else if (replace) { 
+            console.log("Replace item " + alreadyTracked);
+            var keys = Object.keys(vaccine);
+            for (var i= 0; i<keys.length; i++) {
+                // eg.. maintain due/overdue status if scanned...
+                if (vaccine[keys[i]] == null) { vaccine[keys[i]] = $scope.items[alreadyTracked][keys[i]] }  
+            }
+            $scope.$parent.items[alreadyTracked] = vaccine;
+        }
 
     }
 
