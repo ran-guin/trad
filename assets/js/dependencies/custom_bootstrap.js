18a85ddb6886faa1c8bbe2ad17f6fed31a33f05b
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-08-28 17:58:05 -0700

    add custom bootstrap js

---
+++ assets/js/dependencies/custom_bootstrap.js
@@ -0,0 +1,10 @@
+$(document).ready( function()  {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+});
+
